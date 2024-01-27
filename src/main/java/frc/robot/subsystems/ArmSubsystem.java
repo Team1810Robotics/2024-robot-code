7935b9523b2a887a1d472960a82bebcdf7abaec2
gaultpay000 <pgauptney@gmail.com>
2024-01-27 10:06:15 -0600

    added to arm and Pheonix6

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,12 +3,39 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj2.command.TrapezoidProfileSubsystem;
 import frc.robot.Constants.ArmConstants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.ctre.phoenix6.hardware.CANcoder; 
 
 public class ArmSubsystem extends TrapezoidProfileSubsystem {
 
+CANSparkMax motor = new CANSparkMax(0, MotorType.kBrushless);
+    
+CANSparkMax motor2 = new CANSparkMax (0, MotorType.kBrushless);
+
+CANcoder can = new CANcoder(0);
+
     public ArmSubsystem() {
         super(ArmConstants.CONSTRAINTS, ArmConstants.INITIAL_POSITION);
+
+        
+ 
     }
+
+   public void setSpeed(double motorSpeed){    
+        motor.set(motorSpeed);
+}
+
+   public double getposition(){
+        can.getPosition();
+
+        return can.getPosition().getValue();
+}
+
+public void stop(){
+        motor.stopMotor();
+}
 
     @Override
     public void useState(State state) {
