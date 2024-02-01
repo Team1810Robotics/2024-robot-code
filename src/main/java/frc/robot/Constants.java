36f5e1bc0a628cce6f7c6eb111c85669b9dd0f22
Tlesis <jackwilliamson.1215@gmail.com>
2024-01-31 20:33:34 -0600

    yagsl code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,89 +1,25 @@
 package frc.robot;
 
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import java.io.File;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- * For Example:
- * <pre>{@code import static frc.robot.Constants.*; }</pre>
- */
-public final class Constants {
-    // Can IDs set as so
-    // https://drive.google.com/file/d/1fXTWswe1oX0XLHEosK-BxezZ92rvbHfV/view?usp=drive_link
-    public static final class DriveConstants {
-        public static final int PIGEON = 13;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.math.SwerveMath;
 
-        // FIXME: all temporary values and im not sure if this is going to be needed
-        public static final class FR {
-            public static final int STEER = 1;
-            public static final int DRIVE = 2;
-            public static final int CANCODER = 9;
-        }
-
-        public static final class FL {
-            public static final int STEER = 3;
-            public static final int DRIVE = 4;
-            public static final int CANCODER = 10;
-        }
-
-        public static final class BR {
-            public static final int STEER = 5;
-            public static final int DRIVE = 6;
-            public static final int CANCODER = 11;
-        }
-
-        public static final class BL {
-            public static final int STEER = 7;
-            public static final int DRIVE = 8;
-            public static final int CANCODER = 12;
-        }
+public class Constants {
+    public static class IO {
+        public static final double swerveDeadband = 0.3;
+        public static final int driveXAxis = 1;
+        public static final int driveYAxis = 0;
+        public static final int driveOmegaAxis = 2;
+        public static final int resetGyroButton = 9;
+        public static final int driveModeButton = 7;
     }
 
-    public static final class ArmConstants {
-        public static final int MOTOR1_PORT = 0;
-        public static final int MOTOR2_PORT = 0;
-
-        public static final double INITIAL_POSITION = 0.0;
-        public static final double INTAKE_POSITION = 0;
-
-        //TODO: tune values
-        public static final double kP = 0.0; 
-        public static final double kI = 0.0;
-        public static final double kD = 0.0;
-
-        public static final Constraints CONSTRAINTS
-            = new Constraints(0, 0);
-    
-    }
-
-    public static final class IntakeConstants {
-        public static final int MOTOR_PORT = 0;
-        public static final int BEAM_BREAK_PORT = 0;
-    }
-
-    public static final class ShooterConstants {
-        public static final int TOP_MOTOR_PORT = 0;
-        public static final int BOTTOM_MOTOR_PORT = 0;
-    }
-
-    public static final class ExtenderConstants {
-        public static final int MOTOR_PORT = 0;
-        public static final int TOP_LS_PORT = 0;
-        public static final int BOTTOM_LS_PORT = 0;
-    }
-
-    public static final class ClimbConstants {
-        public static final int LEFT_MOTOR_PORT = 0;
-        public static final int RIGHT_MOTOR_PORT = 0;
-
-        public static final int LEFT_TOP_LS = 0;
-        public static final int LEFT_BOTTOM_LS = 0;
-        public static final int RIGHT_TOP_LS = 0;
-        public static final int RIGHT_BOTTOM_LS = 0;
+    public static class Swerve {
+        public static final File directory = new File(Filesystem.getDeployDirectory(), "swerve");
+        public static final double maxVelocity = 5;
+        public static final double driveConversionFactor = SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(4), 6.75, 1024);
+        public static final double angleConversionFactor = SwerveMath.calculateDegreesPerSteeringRotation(12.8, 4096);
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,16 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import frc.robot.Constants.IO;
+import frc.robot.commands.Drivetrain.TeleopDrive;
+import frc.robot.subsystems.swervedrive.Drivetrain;
 
 public class RobotContainer {
-    public RobotContainer() {
-        configureBindings();
-    }
 
-    private void configureBindings() {}
+  private final Drivetrain drivetrain = new Drivetrain();
 
-    public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
-    }
+  CommandJoystick driver = new CommandJoystick(0);
+
+  SendableChooser<Command> autoChooser;
+
+  public RobotContainer() {
+    configureBindings();
+
+    drivetrain.setDefaultCommand(new TeleopDrive(
+      drivetrain,
+      () -> MathUtil.applyDeadband(driver.getRawAxis(IO.driveXAxis), Constants.IO.swerveDeadband),
+      () -> MathUtil.applyDeadband(driver.getRawAxis(IO.driveYAxis), Constants.IO.swerveDeadband),
+      () -> MathUtil.applyDeadband(driver.getRawAxis(IO.driveOmegaAxis), Constants.IO.swerveDeadband),
+      () -> !driver.button(IO.driveModeButton).getAsBoolean()));
+    
+
+    autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Chooser", autoChooser);
+  }
+
+
+  private void configureBindings() {
+    driver.button(IO.resetGyroButton).onTrue(new InstantCommand(drivetrain::zeroGyro));
+  }
+
+  public Command getAutonomousCommand() {
+    return autoChooser.getSelected();
+  }
 }
