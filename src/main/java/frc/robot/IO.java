e1042edde2c41d5ac56e9b046046ec6d8a14224c
Tlesis <jackwilliamson.1215@gmail.com>
2024-02-24 14:26:49 -0600

    another test of pre-commit

--- src/main/java/frc/robot/IO.java
+++ src/main/java/frc/robot/IO.java
@@ -40,26 +40,16 @@ public final class IO {
     public static final JoystickButton rightJoystick_button11 = new JoystickButton(leftJoystick, 11);
 
     // Xbox buttons
-    public static final JoystickButton manipulatorXbox_A =
-            new JoystickButton(xboxController, XboxController.Button.kA.value);
-    public static final JoystickButton manipulatorXbox_B =
-            new JoystickButton(xboxController, XboxController.Button.kB.value);
-    public static final JoystickButton manipulatorXbox_X =
-            new JoystickButton(xboxController, XboxController.Button.kX.value);
-    public static final JoystickButton manipulatorXbox_Y =
-            new JoystickButton(xboxController, XboxController.Button.kY.value);
-    public static final JoystickButton manipulatorXbox_LB =
-            new JoystickButton(xboxController, XboxController.Button.kLeftBumper.value);
-    public static final JoystickButton manipulatorXbox_RB =
-            new JoystickButton(xboxController, XboxController.Button.kRightBumper.value);
-    public static final JoystickButton manipulatorXbox_Back =
-            new JoystickButton(xboxController, XboxController.Button.kBack.value);
-    public static final JoystickButton manipulatorXbox_Start =
-            new JoystickButton(xboxController, XboxController.Button.kStart.value);
-    public static final JoystickButton manipulatorXbox_LStick =
-            new JoystickButton(xboxController, XboxController.Button.kLeftStick.value);
-    public static final JoystickButton manipulatorXbox_RStick =
-            new JoystickButton(xboxController, XboxController.Button.kRightStick.value);
+    public static final JoystickButton manipulatorXbox_A = new JoystickButton(xboxController, XboxController.Button.kA.value);
+    public static final JoystickButton manipulatorXbox_B = new JoystickButton(xboxController, XboxController.Button.kB.value);
+    public static final JoystickButton manipulatorXbox_X = new JoystickButton(xboxController, XboxController.Button.kX.value);
+    public static final JoystickButton manipulatorXbox_Y = new JoystickButton(xboxController, XboxController.Button.kY.value);
+    public static final JoystickButton manipulatorXbox_LB = new JoystickButton(xboxController, XboxController.Button.kLeftBumper.value);
+    public static final JoystickButton manipulatorXbox_RB = new JoystickButton(xboxController, XboxController.Button.kRightBumper.value);
+    public static final JoystickButton manipulatorXbox_Back = new JoystickButton(xboxController, XboxController.Button.kBack.value);
+    public static final JoystickButton manipulatorXbox_Start = new JoystickButton(xboxController, XboxController.Button.kStart.value);
+    public static final JoystickButton manipulatorXbox_LStick = new JoystickButton(xboxController, XboxController.Button.kLeftStick.value);
+    public static final JoystickButton manipulatorXbox_RStick = new JoystickButton(xboxController, XboxController.Button.kRightStick.value);
 
     private IO() {
         /* what does sleep feel like */
