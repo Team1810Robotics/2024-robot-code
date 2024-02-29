ca33d85d9a95181f53f05ddf93099e05bc489f86
Tlesis <jackwilliamson.1215@gmail.com>
2024-02-28 20:41:36 -0600

    vision pose stuff (to test)

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,65 +1,46 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.VisionConstants;
-import java.util.List;
-import java.util.Optional;
-import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class VisionSubsystem extends SubsystemBase {
-    private static PhotonCamera camera = new PhotonCamera(VisionConstants.CAMERA_NAME);
-    private Transform3d robotToCam =
-            new Transform3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d(0, 0, 0));
+    private double previousPipelineTimestamp = 0.0;
 
-    private PhotonPoseEstimator photonPoseEstimator =
-            new PhotonPoseEstimator(
-                    VisionConstants.APRIL_TAG_FIELD_LAYOUT,
-                    PoseStrategy.CLOSEST_TO_REFERENCE_POSE,
-                    camera,
-                    robotToCam);
+    private PhotonCamera photonCamera;
+    private SwerveSubsystem drive;
 
-    public PhotonPipelineResult getResult() {
-        return camera.getLatestResult();
+    private Field2d field = new Field2d();
+
+    public VisionSubsystem(SwerveSubsystem drive) {
+        this.drive = drive;
+
+        this.photonCamera = new PhotonCamera(VisionConstants.CAMERA_NAME);
     }
 
-    public boolean hasTarget() {
-        return getResult().hasTargets();
-    }
+    @Override
+    public void periodic() {
+        PhotonPipelineResult pipelineResult = photonCamera.getLatestResult();
+        double resultTimestamp = pipelineResult.getTimestampSeconds();
 
-    public List<PhotonTrackedTarget> getTargets() {
-        return getResult().getTargets();
-    }
+        if (resultTimestamp == previousPipelineTimestamp && !pipelineResult.hasTargets()) return;
+        previousPipelineTimestamp = resultTimestamp;
+        PhotonTrackedTarget target = pipelineResult.getBestTarget();
+        int fiducialId = target.getFiducialId();
 
-    public double getPoseFromTarget() {
-        PhotonTrackedTarget target = getTargets().get(0);
-        double range =
-                PhotonUtils.calculateDistanceToTargetMeters(
-                        VisionConstants.CAMERA_HEIGHT,
-                        VisionConstants.APRILTAG_RED_SHOOTER_HEIGHT,
-                        VisionConstants.CAMERA_PITCH,
-                        Units.degreesToRadians(target.getPitch()));
+        if (target.getPoseAmbiguity() >= VisionConstants.APRILTAG_AMBIGUITY_THRESHOLD) return;
+        var targetPose = VisionConstants.APRIL_TAG_FIELD_LAYOUT.getTagPose(fiducialId);
+        Transform3d camToTarget = target.getBestCameraToTarget();
+        Pose3d camPose = targetPose.get().transformBy(camToTarget.inverse());
 
-        return range;
-    }
+        var visionMeasurement = camPose.transformBy(VisionConstants.CAMERA_TO_ROBOT);
+        drive.addVisionMeasurement(visionMeasurement.toPose2d(), resultTimestamp);
 
-    double getRangeMeters() {
-        double range = PhotonUtils.calculateDistanceToTargetMeters(0, 0, 0, 0);
-        return range;
-    }
-
-    public Pose3d getRobotPose() {
-        Optional<EstimatedRobotPose> pose = photonPoseEstimator.update();
-        return pose.get().estimatedPose;
+        field.setRobotPose(drive.getPose());
     }
 }
